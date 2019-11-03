552a5af2e3374ae9777d77b7f932b60eb7accde3
liuxinqiong <372513646@qq.com>
2019-11-04 02:33:03 +0800

    update vue part

--- vue/dev/min-vuex.js
+++ vue/dev/min-vuex.js
@@ -1,11 +1,43 @@
 import Vue from 'vue'
 
+let Vue;
+function install(_Vue) {
+    Vue = _Vue;
+    function vuexInit() {
+        var options = this.$options;
+        if(options.store) {
+            this.$store = typeof options.store === 'function'
+            ? options.store()
+            : options.store
+        } else if(options.parent && options.parent.$store) {
+            this.$store = options.parent.$store；
+        }
+    }
+    Vue.mixin({
+        beforeCreate: vuexInit
+    })
+}
+
 const Store = function Store(option = {}) {
-    const {state = {}, mutations={}} = options
+    const {state = {}, mutations={}, getters={}} = options
+    const computed = {}
+    const store = this
+    store.getters = {}
+    for(let [key, fn] of Object.entries(getters)) {
+        computed[key] = function() {
+            return fn(store.state, store.getters)
+        }
+        Object.defineProperties(store.getters, key, {
+            get: function() {
+                return store._vm[key]
+            }
+        })
+    }
     this._vm = new Vue({
         data: {
             $$state: state
-        }
+        },
+        computed
     })
     this._mutation = mutations
 }
@@ -24,4 +56,4 @@ Object.defineProperties(Store.prototype, {
     }
 })
 
-export default {Store}
+export default {Store, install}
