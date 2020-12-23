efc201aef8afc7f95f376ca45c0f7f88b6370b73
xinqiong <lxq@xkool.info>
2020-12-23 15:44:39 +0800

    update

--- prepare/todo.md
+++
@@ -1,9 +0,0 @@
-面试准备的 TODO 清单
-* ~~HTML5 离线缓存 / service worker~~
-* ~~CSS IFC~~
-* ~~Vue 技术栈复习~~
-* webpack 复习
-* 项目难点：无限列表滚动优化
-* Vue 源码阅读，但是整体讲实在是太多了，可以说重点看过哪些部分
-  * 响应式原理等
-  * Observer Watcher Dep 的关系
--- vue/dev/min-vuex.js
+++ vue/dev/min-vuex.js
@@ -1,59 +1,58 @@
 import Vue from 'vue'
 
-let Vue;
+let Vue
 function install(_Vue) {
-    Vue = _Vue;
-    function vuexInit() {
-        var options = this.$options;
-        if(options.store) {
-            this.$store = typeof options.store === 'function'
-            ? options.store()
-            : options.store
-        } else if(options.parent && options.parent.$store) {
-            this.$store = options.parent.$store；
-        }
+  Vue = _Vue
+  function vuexInit() {
+    var options = this.$options
+    if (options.store) {
+      this.$store =
+        typeof options.store === 'function' ? options.store() : options.store
+    } else if (options.parent && options.parent.$store) {
+      this.$store = options.parent.$store
     }
-    Vue.mixin({
-        beforeCreate: vuexInit
-    })
+  }
+  Vue.mixin({
+    beforeCreate: vuexInit,
+  })
 }
 
 const Store = function Store(option = {}) {
-    const {state = {}, mutations={}, getters={}} = options
-    const computed = {}
-    const store = this
-    store.getters = {}
-    for(let [key, fn] of Object.entries(getters)) {
-        computed[key] = function() {
-            return fn(store.state, store.getters)
-        }
-        Object.defineProperties(store.getters, key, {
-            get: function() {
-                return store._vm[key]
-            }
-        })
+  const { state = {}, mutations = {}, getters = {} } = options
+  const computed = {}
+  const store = this
+  store.getters = {}
+  for (let [key, fn] of Object.entries(getters)) {
+    computed[key] = function () {
+      return fn(store.state, store.getters)
     }
-    this._vm = new Vue({
-        data: {
-            $$state: state
-        },
-        computed
+    Object.defineProperties(store.getters, key, {
+      get: function () {
+        return store._vm[key]
+      },
     })
-    this._mutation = mutations
+  }
+  this._vm = new Vue({
+    data: {
+      $$state: state,
+    },
+    computed,
+  })
+  this._mutation = mutations
 }
 
-Store.prototype.commit = function(type, payload) {
-    if(this._mutation[type]){
-        this._mutation[type](this.state, payload)
-    }
+Store.prototype.commit = function (type, payload) {
+  if (this._mutation[type]) {
+    this._mutation[type](this.state, payload)
+  }
 }
 
 Object.defineProperties(Store.prototype, {
-    state: {
-        get: function() {
-            return this._vm._data.$$state
-        }
-    }
+  state: {
+    get: function () {
+      return this._vm._data.$$state
+    },
+  },
 })
 
-export default {Store, install}
+export default { Store, install }
